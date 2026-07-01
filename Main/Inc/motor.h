4bd9e4b8f93255094598357fc5a756f1ae9b6ca1
Kim Tae Hwan <kth5931@gmail.com>
2026-07-01 22:16:21 +0900

    Update menu / Add loading last function

--- Main/Inc/motor.h
+++ Main/Inc/motor.h
@@ -8,11 +8,15 @@
 #ifndef INC_MOTOR_H_
 #define INC_MOTOR_H_
 
+#define INV_TIRE_RADIUS	(2.f / 0.023f)
+
 typedef enum {
     FOC_MODE_NO_SVPWM_SPIN  = 0, // 모터 구동 O, SVPWM 연산 X (단순 PWM/수동 제어)
     FOC_MODE_SVPWM_NO_SPIN  = 1, // 모터 구동 X, SVPWM 연산 O (전류/각도 연산 디버깅용)
     FOC_MODE_SVPWM_SPIN     = 2  // 모터 구동 O, SVPWM 연산 O (정상 FOC 제어)
 } FOC_DriveMode_t;
+
+
 
 void MTR_Read_Register(void);
 void MTR_Simple_Control(void);
@@ -21,5 +25,7 @@ void MTR_Simple_FOC(void);
 void MTR_Encoder_Test(void);
 void MTR_Speed_FOC(void);
 void MTR_Current_Tune_Loop();
+
+void Motor_Set_Speed(float mps_L, float mps_R);
 
 #endif /* INC_MOTOR_H_ */
